5313772492fff77e4703d11fffc0ff2e6afc6cc9
shambu1329 <shambukrishna34@gmail.com>
2022-12-03 13:59:03 +0530

    completed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,33 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AddproductComponent } from './addproduct/addproduct.component';
+import { ViewallproductComponent } from './viewallproduct/viewallproduct.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http'
+
+
+
+const myroutes=[
+  {path:"",component:AddproductComponent},
+  {path:"Viewallproduct",component:ViewallproductComponent}
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddproductComponent,
+    ViewallproductComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(myroutes),
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
